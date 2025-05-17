7506e33d9319c651c39ec9a3742326aa650c0c53
sandeep-luhaniwal <sandeepluh0001.com>
2025-05-17 15:27:29 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+// Connect to DB and start server
+connectDB().then(() => {
+  app.listen(process.env.PORT, () =>
+    console.log(`Server running on port ${process.env.PORT}`)
+  );
+});
